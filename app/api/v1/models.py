bcd48beaa559a65b78ab1864e6b14d2fef69d0d8
Constantine <asirvex@gmail.com>
2018-10-17 15:16:30 +0300

    [#161282897] Add store-attendant and admin classes

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -5,30 +5,49 @@ sales = []
 
 class StoreAttendant():
     """creates a store attendant object"""
-    def __init__(self, employee_id, name, password):
+    def __init__(self, employee_id, username, first_name, second_name, password):
         self.employee_id = employee_id
-        self.name = name
+        self.username = username
+        self.first_name = first_name
+        self.second_name = second_name
         self.password = password
         self.admin = False
 
-    def get_name(self):
-        return self.name
+    def get_username(self):
+        return self.username
+
+    def get_first_name(self):
+        return self.first_name
+    
+    def get_second_name(self):
+        return self.second_name
 
     def get_employee_id(self):
-        self.employee_id
+        return self.employee_id
     
     def get_password(self):
         return self.password
+
+    def get_admin_status(self):
+        return self.admin
     
 
 class Admin(StoreAttendant):
     """creates an admin object"""
-    def __init__(self, employee_id, name, password):
-        super().__init__(self, employee_id, name, password)
+    def __init__(self, employee_id, username, first_name, second_name, password):
+        super().__init__(employee_id, username, first_name, second_name, password)
         self.admin = True
 
-    def add_store_attendant(self, store_attendant):
-        products.append(store_attendant)
+    def add_store_attendant(self, employee_id, username, first_name, second_name, password):
+    """Creates a store_attedant object and adds it to the store attendants list"""
+        if username in store_attendants:
+            return "username already taken"
+        for employee in store_attendants:
+            if employee.get_employee_id() == employee_id:
+                return "employee id already taken"
+        username = StoreAttendant(employee_id, username, first_name, second_name, password)
+        store_attendants.append(username)
+        return "store attendant created successfully"
 
 class Products():
     def __init__(self, id, name, description, price):
