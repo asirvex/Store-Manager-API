e226eb8eed03f99b59082c19300415a2e89cfd57
asirvex <asirvex@gmail.com>
2018-10-22 20:18:52 +0300

    [#161391183] Add endpoint and functionality for getting a specific product

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
 from flask import Blueprint
-from .v1.views import Products, SpecificProduct, Sales, Login, SignUp
+from .v1.views import Products, SpecificProduct, Sales, Login, SignUp, SpecificSale
+
 
 v1 = Blueprint("version_one", __name__)
 api = Api(v1)
@@ -9,3 +10,4 @@ api.add_resource(SpecificProduct, "/products/<product_id>")
 api.add_resource(Sales, "/sales")
 api.add_resource(Login, "/auth/login")
 api.add_resource(SignUp, "/auth/signup")
+api.add_resource(SpecificSale, "/sales/<sale_id>")
